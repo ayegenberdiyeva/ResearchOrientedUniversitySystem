23b22267086982761181dc5426502431854247d8
Amina <aminayegnb@gmail.com>
2024-12-21 09:52:24 +0500

    Added methods for library

--- Model/Users/Librarian.java
+++ Model/Users/Librarian.java
@@ -1,12 +1,14 @@
 package Users;
 
 import Stuff.Book;
+import Utils.LanguageManager;
 
 import java.util.*;
 
 public class Librarian extends User {
     public Librarian(String firstName, String lastName, String email, String password) {
         super(firstName, lastName, email, password);
+        this.inventory = new HashMap<>();
     }
 
     private Map<Book, Integer> inventory;
@@ -17,21 +19,41 @@ public class Librarian extends User {
     }
 
     public void addBook(Book book, int quantity) {
-        // TODO implement here
+        if (book == null || quantity <= 0) {
+            System.out.println(LanguageManager.getMessage("invalid_book_or_quantity"));
+            return;
+        }
+        inventory.put(book, inventory.getOrDefault(book, 0) + quantity);
+        System.out.println(LanguageManager.getMessage("book_added", book.getTitle(), quantity));
     }
 
     public void removeBook(Book book) {
-        // TODO implement here
+        if (!inventory.containsKey(book)) {
+            System.out.println(LanguageManager.getMessage("book_not_found", book.getTitle()));
+            return;
+        }
+        inventory.remove(book);
+        System.out.print(LanguageManager.getMessage("book_removed", book.getTitle()));
     }
 
     public boolean borrowBook(Student student, Book book) {
-        // TODO implement here
-        return false;
+        if (!inventory.containsKey(book) || inventory.get(book) <= 0) {
+            System.out.println(LanguageManager.getMessage("book_unavailable", book.getTitle()));
+            return false;
+        }
+        inventory.put(book, inventory.get(book) - 1);
+        System.out.println((LanguageManager.getMessage("book_borrowed", book.getTitle())));
+        return true;
     }
 
     public boolean returnBook(Student student, Book book) {
-        // TODO implement here
-        return false;
+        if (!inventory.containsKey(book)) {
+            inventory.put(book, 1);
+        } else {
+            inventory.put(book, inventory.get(book) + 1);
+        }
+        System.out.println(LanguageManager.getMessage("book_returned", student.getId(), book.getTitle()));
+        return true;
     }
 
     public Map<Book, Integer> getInventory() {
