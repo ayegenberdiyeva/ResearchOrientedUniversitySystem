528c20aa98d0248f776cbe5a1ec472587cdd3248
Amina <aminayegnb@gmail.com>
2024-12-21 09:52:24 +0500

    Added methods for library

--- Model/Stuff/Book.java
+++ Model/Stuff/Book.java
@@ -1,42 +1,48 @@
 package Stuff;
 
-/**
- * 
- */
 public class Book {
-
-    /**
-     * Default constructor
-     */
-    public Book() {
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
     }
 
-    /**
-     * 
-     */
     private String title;
-
-    /**
-     * 
-     */
     private String author;
-
-    /**
-     * 
-     */
     private String isbn;
 
-    /**
-     * 
-     */
-    private int quantity;
+    public String getTitle() {
+        return title;
+    }
 
-    /**
-     * @return
-     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
     public String toString() {
-        // TODO implement here
-        return "";
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getName()).append(" {");
+        sb.append("title='").append(title).append('\'');
+        sb.append(", author='").append(author).append('\'');
+        sb.append(", isbn='").append(isbn).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 
 }
